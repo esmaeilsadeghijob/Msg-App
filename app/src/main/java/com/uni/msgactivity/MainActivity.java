a4c30dafb4e6277a54386c1a7eff44b073ca28f3
e.sadeghi <e.sadeghi@fwutech.com>
2023-05-29 10:36:05 -0400

    Refactoring

--- app/src/main/java/com/uni/msgactivity/MainActivity.java
+++ app/src/main/java/com/uni/msgactivity/MainActivity.java
@@ -2,7 +2,10 @@ package com.uni.msgactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view){
+        //1
+        EditText editText = (EditText)findViewById(R.id.editText_1);
+        String name = editText.getText().toString();
+
+        //2
+        Intent intent = new Intent(this, MsgActivity.class);
+        intent.putExtra("NAME", name);
+
+        //3
+        startActivity(intent);
+
     }
 }
